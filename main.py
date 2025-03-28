265d8a6927b9efe60158789ce122cc12fede6f1d
Sergio Sillero Moreno <ssillerom@MacBook-Pro-de-Sergio.local>
2025-03-28 21:40:22 +0100

    init push

---
+++ main.py
@@ -0,0 +1,28 @@
+from browser_use import Agent, Browser, BrowserConfig
+from langchain_openai import ChatOpenAI
+import asyncio
+# Configure the browser to connect to your Chrome instance
+browser = Browser(
+    config=BrowserConfig(
+        # Specify the path to your Chrome executable
+        chrome_instance_path='/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',  # macOS path
+        # For Windows, typically: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+        # For Linux, typically: '/usr/bin/google-chrome'
+    )
+)
+
+# Create the agent with your configured browser
+agent = Agent(
+    task="Entra en linkedin y haz un post haciendo un post sobre como se puede aplicar los MCP servers para hacer agentes en una aseguradora, usa emojis y hazlo divertido ",
+    llm=ChatOpenAI(model='gpt-4o'),
+    browser=browser,
+)
+
+async def main():
+    await agent.run()
+
+    input('Press Enter to close the browser...')
+    await browser.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
